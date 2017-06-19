4beeeb4c42a27ef5fb3e4db17c9a1933ab8be369
Michael Woolweaver <mwoolweaver@users.noreply.github.com>
2017-06-19 18:10:04 -0500

    more info

--- update.sh
+++ update.sh
@@ -1,13 +1,21 @@
-./package.sh
+# -- Comment out the following line if you just want a package list 
+./package.sh 
+# --
 
-echo Removing Old Compressed Packages File
+# -- Expect this to fail if they haven't been created
+echo Removing Old Packages Files
 rm -r Packages
 rm -r Packages.bz2
 rm -r Packages.gz
+# --
 
+# -- Will scan the current directory and all sub directories for .deb files
 echo Creating New Packages file
 dpkg-scanpackages -m . /dev/null >Packages
+# -- Also will require installing 'dpkg-scanpackages' via homebrew if on macOS/OS X
 
+# -- Provide both Bzip2 and gzip
 echo Compressing New Packages List
-Bzip2 -fks Packages
-gzip -c Packages > Packages.gz
+Bzip2 -fks Packages # Cydia prefers Bzip2 but
+gzip -c Packages > Packages.gz # gzip is used as a fallback
+# --
